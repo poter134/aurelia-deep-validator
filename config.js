be571ba771c60ad74dd318a5047623969a9bac36
Peter Lea <plea@meliorsolutions.com>
2017-04-27 09:03:26 +0100

    added a config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+System.config({
+    defaultJSExtensions: true,
+    transpiler: false,
+    paths: {
+        "github:*": "jspm_packages/github/*",
+        "npm:*": "jspm_packages/npm/*"
+    }
+});
