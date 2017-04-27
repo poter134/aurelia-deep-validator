ed8c093367b452054fc65ec8b9d13da7dcb1910f
Peter Lea <plea@meliorsolutions.com>
2017-04-27 09:16:56 +0100

    added package.json

--- config.js
+++
@@ -1,8 +0,0 @@
-System.config({
-    defaultJSExtensions: true,
-    transpiler: false,
-    paths: {
-        "github:*": "jspm_packages/github/*",
-        "npm:*": "jspm_packages/npm/*"
-    }
-});
